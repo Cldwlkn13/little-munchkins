b4596451f8840c63584ae270059eef1ecb18109b
Cldwlkn13 <gavin_rudge@hotmail.co.uk>
2021-03-30 20:59:02 +0000

    added builder method steps

--- static/js/script.js
+++ static/js/script.js
@@ -25,20 +25,13 @@ $(document).ready(function () {
     slideAndFadeIn("#home-info-text-2", 2000, 1500);
     slideAndFadeIn("#home-info-text-3", 2000, 3000);
 
-    $('#ingredients-tab, #desc-tab, #method-tab').click(function(){
-        
+    $('#ingredients-tab, #desc-tab, #method-tab').click(function(){        
         var selector = $(this).get(0).id.toString().replace("-tab","");
-        
         $(this).addClass("active grey darken-3");
-
         $(this).parent().siblings(".tab").find(".tab-link").removeClass("active grey darken-3");
-
         var tabs = $(this).parent().parent().parent().next();
-
         var tab = tabs.children("#" + selector);
-        
         tab.css("display", "block");
-
         tab.siblings().css("display", "none");
     });
 
@@ -78,4 +71,31 @@ $(document).ready(function () {
     setTimeout(function() {
         $('.flashes').fadeOut('slow');
     }, 2000);
+
+    $('#builder-add-step').click(function() {      
+        var countsteps = $('.steps .step').length;
+        console.log(countsteps)
+        var i = countsteps + 1;
+
+        var step = `<div class='row step' id='step-${i}'>` +
+                        "<div class='input-field col m2'>" +
+                            `<select id='step-${i}-type'>` +
+                                "<option value='1' selected>Prepare</option>" +
+                                "<option value='2'>Cook</option>" +
+                            "</select>" +
+                        "</div>" +
+                        "<div class='input-field col m8'>" +
+                            `<input type='text' id='step-${i}-desc' name='step-${i}-desc' minlength='1' maxlength='100' class='validate' required />` +
+                            `<label for='step-${i}-desc'>Describe Step ${i}</label>` +
+                        "</div>" +
+                        "<div class='input-field col m2'>" +
+                            `<input type='text' id='time-input-${i}' name='time-input-${i}'  minlength='1' maxlength='3' class='validate' pattern='^[0-9]{1,3}$' required />` +
+                            `<label for='time-input-${i}'>Time</label>` +
+                        "</div>" +
+                    "</div>"    
+        
+        $('.steps').append(step); 
+
+        $(`#step-${i}-type`).formSelect();
+    });
 });
